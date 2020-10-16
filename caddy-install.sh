5feeeca64da54cc58b819ef4b649e88b5db4c254
Radek Osmulski <rosmulski@gmail.com>
2020-10-16 14:31:41 +0200

    fix caddy-install.sh

--- caddy-install.sh
+++ caddy-install.sh
@@ -1,5 +1,5 @@
 { echo "$NEWPASS"; echo "deb [trusted=yes] https://apt.fury.io/caddy/ /"; } | sudo -k -S tee -a /etc/apt/sources.list.d/caddy-fury.list
 echo $NEWPASS | sudo -k -S apt update
 echo $NEWPASS | sudo -k -S apt install -y caddy
-sudo ufw allow http
-sudo ufw allow https
+echo $NEWPASS | sudo -k -S ufw allow http
+echo $NEWPASS | sudo -k -S ufw allow https
