ee7818d1ea7e1ca77975d0b101fe257f602dd8ce
Jeremy Howard <github@jhoward.fastmail.fm>
2024-12-08 05:38:59 +1000

    miniforge

--- setup-conda.sh
+++ setup-conda.sh
@@ -4,15 +4,15 @@ set -eou pipefail
 case "$OSTYPE" in
   darwin*)
     case $(uname -m) in
-      arm64)  DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh; ;;
-      *)      DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh; ;;
+      arm64)   DOWNLOAD=https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh; ;;
+      *)       DOWNLOAD=https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-x86_64.sh; ;;
     esac ;;
   linux*)
     case $(uname -m) in
-      aarch64) DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh; ;;
-      *)       DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh; ;;
+      aarch64) DOWNLOAD=https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh; ;;
+      *)       DOWNLOAD=https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh; ;;
       esac ;;
-  *)          echo "unknown: $OSTYPE" ;;
+  *)           echo "unknown: $OSTYPE" ;;
 esac
 
 case "$SHELL" in
@@ -22,11 +22,11 @@ case "$SHELL" in
   *)        echo "unknown: $SHELL" ;;
 esac
 
-echo Downloading installer...
-curl -LO --no-progress-meter $DOWNLOAD
-bash Miniconda3-*.sh -b
+echo Downloading $DOWNLOAD
+wget $DOWNLOAD
+bash Miniforge3-*.sh -b
 
-~/miniconda3/bin/conda init $SHELL_NAME
+~/miniforge3/bin/conda init $SHELL_NAME
 
 echo Please close and reopen your terminal.
 
